04ddf94b06560897fc7f37f36f8706ac4d316e3b
Minh Nguyen <minhanh.nguyenquoc@gmail.com>
2023-06-12 15:35:14 -0400

    update logo

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -69,7 +69,7 @@ const Auth = () => {
                 />
             </Head>
 
-            <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
+            <div className="relative h-full w-full bg-[url('/images/hero2.png')] bg-no-repeat bg-fixed bg-cover">
                 <div className="bg-black w-full h-full lg:bg-opacity-50">
                     <nav className="px-6 md:px-12 py-5">
                         <img src="/images/logo.png" alt="Logo" className="h-12"/>
